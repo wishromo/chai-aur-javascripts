c85cce2ddb9f4b0079da6afd600abc187070cf83
wishromo <hinasaeed1212@gmail.com>
2025-05-20 18:16:34 +0000

    conversion of datatype

---
+++ .devcontainer/01_basic/conversion.js
@@ -0,0 +1,23 @@
+let score='Hina'
+ //console.log(typeof(score));
+//  console.log(typeof score);
+let valueinNumber=Number(score)
+// console.log(typeof valueinNumber);
+// console.log(valueinNumber);//give NaN beacause
+//'33'=>33
+//"33abc"=> Nan
+//true=>1;false=>0
+let isloggedIn ="Hina"
+ booleanIsloggedIN = Boolean(isloggedIn)
+ console.log(booleanIsloggedIN) 
+// 1=>true;0=>false
+// "" => false
+// "hina" => true
+
+let StringNumber=33
+ StringNumber=String(StringNumber)
+console.log(StringNumber);
+console.log(typeof StringNumber);
+
+
+
