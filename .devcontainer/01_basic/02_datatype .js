c967377f35afff772499072207b2f50d4cde4391
wishromo <hinasaeed1212@gmail.com>
2025-05-20 17:41:09 +0000

    datatype

---
+++ .devcontainer/01_basic/02_datatype .js
@@ -0,0 +1,28 @@
+//treat all js code as newer version
+'use strict';
+//alert(3+3)//we are using nodejs,not browser
+console.log(3 
+    +3);//code readibility should be high
+console.log('Hina')  
+let name='Hina'
+let age=18
+let isloggedIn=false
+let state
+// let state=null  other way to write
+//number => 2 to power 53
+//bigint use for infinity of number
+//string =>""
+//boolean=>true/false
+//null standalone value
+// undefined
+//symbol=> using for react to find uniqueness use for figma
+
+
+//object
+console.log(typeof'Hina')
+console.log(typeof undefined);
+console.log(typeof null);
+
+
+
+ 
