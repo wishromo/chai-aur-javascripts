177a76cadb744a175524237a4ce5249939752d32
wishromo <hinasaeed1212@gmail.com>
2025-05-25 18:23:34 +0000

    string datatype further types

---
+++ .devcontainer/01_basic/05-string.js
@@ -0,0 +1,35 @@
+ const name="hina"
+ const repoCount=1
+ //console.log(name+repoCount+"value")
+ //console.log(`hello my name is ${name} and my repo is ${repoCount}`);
+ const gameName=new String('Hina_saeed') // will see in cosole to see that type copy it and paste in inspect and console
+// console.log(gameName)
+// console.log(gameName[0]);//give first character of the string
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt('2'));//give the no that what is on that line
+// console.log(gameName.indexOf('t'));//give the positon og the character
+const newString=gameName.substring(0,5)
+// console.log(newString);
+const anotherstring=gameName.slice(-8,4)
+// console.log(anotherstring);
+const newString1="    hina.   "
+// console.log(newString1);
+// console.log(newString1.trim());
+const url="http://hitesh.com/hitesh%20choudhary"
+// console.log(url.replace('%20',"-"))
+// console.log(url.includes('sundar'))//tell us that is included in it or not
+console.log(gameName.split('_'));//change th e variable in array
+
+
+
+
+
+
+
+
+
+
+
+ 
