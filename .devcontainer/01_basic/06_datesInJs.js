d331aab15e5b5b75c6083e18eb61d9174351cead
wishromo <hinasaeed1212@gmail.com>
2025-05-25 19:49:05 +0000

    maths and Number

---
+++ .devcontainer/01_basic/06_datesInJs.js
@@ -0,0 +1,20 @@
+//Date is an object in js
+
+let MyDate=new Date()
+// console.log(MyDate.toString());
+// console.log(MyDate.toDateString())
+// console.log(MyDate.toISOString())
+// console.log(MyDate.toLocaleString())
+//console.log(typeof MyDate);
+
+
+// let  myCreateDate=new Date(2025,0,22)//give the day at january 22 2025
+// console.log(myCreateDate.toDateString());//withn this commond
+//let  myCreateDate=new Date(2025,0,22,5,3)
+// console.log(myCreateDate.toLocaleString());
+// let myCreateDate=new Date("20225-01-14")
+let myCreateDate=new Date("01-14-2023")
+console.log(myCreateDate.toLocaleString());
+
+
+
---
+++ .devcontainer/01_basic/numsAndMaths.js
@@ -0,0 +1,45 @@
+const score=100
+const balance=new Number(100)
+// console.log(balance);
+// console.log(balance.toString().length);//give us a lenght of the variable value
+// console.log(balance.toFixed(2));//to give precision in value means add decimal and fixd it to 2 use in ecoonerce website
+const otherNumber=23.456474
+// console.log(otherNumber.toPrecision(3)); //give only 23.4
+const otherNumber1=123.45678
+// console.log(otherNumber1.toPrecision(4))//count from the start and give 4 values
+const hunderds=10000000
+// console.log(hunderds.toLocaleString('en-IN'));//convert the number to indian style or pakistani stuyle of the monery
+// **********************Maths*********/
+// console.log(Maths);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6))
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4,9));
+// console.log(Math.min(4,3,6,8));
+// console.log(Math.max(4,3,6,8));
+// console.log(Math.random());//give value fromm 0 1and 1
+// console.log(Math.random()+10);//add the 10 in 0 and give the value
+
+// console.log(Math.random()+10+1);//add 10 and then 1 in 0 before decimal
+// console.log((Math.random()+10)+1);
+// console.log(Math.floor(Math.random()+10)+1);
+const min=10
+const max=20
+console.log(Math.random(max-min+1))
+console.log(Math.random((max-min+1)+min))
+console.log(Math.floor(Math.random((max-min+1)+min)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
