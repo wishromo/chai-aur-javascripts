1ff64cd344fe5493635dc246c08e498264e097a2
wishromo <hinasaeed1212@gmail.com>
2025-05-25 12:53:27 +0000

    conversion of datatype

--- .devcontainer/01_basic/conversion.js
+++ .devcontainer/01_basic/conversion.js
@@ -1,7 +1,11 @@
 let score='Hina'
+ //const score=req.body cannot give the actual type of the data in variable 
+
+
  //console.log(typeof(score));
 //  console.log(typeof score);
 let valueinNumber=Number(score)
+//ginve garuntee that it is number Number(score)
 // console.log(typeof valueinNumber);
 // console.log(valueinNumber);//give NaN beacause
 //'33'=>33
@@ -9,15 +13,18 @@ let valueinNumber=Number(score)
 //true=>1;false=>0
 let isloggedIn ="Hina"
  booleanIsloggedIN = Boolean(isloggedIn)
- console.log(booleanIsloggedIN) 
+// console.log(booleanIsloggedIN) 
 // 1=>true;0=>false
 // "" => false
 // "hina" => true
 
-let StringNumber=33
- StringNumber=String(StringNumber)
-console.log(StringNumber);
-console.log(typeof StringNumber);
+let SomeNumber=33
+let StringNumber=String(SomeNumber)
+// console.log(StringNumber);
+// console.log(typeof StringNumber);
 
-
+/****************Operation ***********/
+let value =3;
+let negValue= -value;
+console.log(negValue);  
 
