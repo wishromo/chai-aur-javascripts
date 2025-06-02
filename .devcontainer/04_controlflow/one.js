89e8453eda2c296ba9bca8d5d8408f5299125c5e
wishromo <hinasaeed1212@gmail.com>
2025-06-02 09:16:59 +0000

    understand the control flow of if else

---
+++ .devcontainer/04_controlflow/one.js
@@ -0,0 +1,47 @@
+//if
+// if(ture){ // meand the condtion inside the code is true then excute from line no 3 to 5
+
+
+// }
+// if(false){// mean theat if the condition is false do no texecute from line no 6 to 11
+
+
+
+
+// }
+ const isUserLoggedIn= true
+if (isUserLoggedIn){}
+//<,>,<=,>=,==,!=
+// 2<=2 ,3!=2 3 not equal 2
+const balance = 1000
+// if(balance >500); console.log("test");console.log("test2")
+// if (balance<500){
+//     console.log("less then 500");
+    
+// }
+// else if (balance<750) { 
+
+//     console.log("less then 750");
+    
+// }
+// else if (balance<=900) {
+//     console.log("less then 1000");
+    
+// }
+// else { console.log("less then 1200")}
+const userLoggedIn =true
+const debitCard =true
+const loggedInFromGoogle=false
+cosnt loggedInFromEmail=true
+if(userLoggedIn && debitCard && 2==3){
+    console.log("allow to buy courses");
+    
+}
+if (loggedInFromGoogle ||loggedInFromEmail ){//  const user= true if (loggedInFromGoogle ||loggedInFromEmail || user)//can be write like this as well
+    console.log("user logged in");
+    
+
+}
+
+ 
+
