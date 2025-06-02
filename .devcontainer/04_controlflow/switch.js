51a5342d6a09da674f665733b43c54ca69c1075c
wishromo <hinasaeed1212@gmail.com>
2025-06-02 09:59:46 +0000

    SWITCH LEARNING

---
+++ .devcontainer/04_controlflow/switch.js
@@ -0,0 +1,34 @@
+// switch (key) {. //basic syntax
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }//use for long checking like months we can not write if else for every month
+// 
+const month="march"
+switch (month) {
+    case "jan":
+        console.log("jan")
+        break;
+    case "feb":
+      console.log("feb")
+        break;
+    case "march":
+        console.log("marcg")
+        break;
+    case "april":
+        console.log("april")
+        break;
+    case "may":
+        console.log("may")
+        break;
+    case june:
+        console.log("june")
+        break;
+
+    default:
+        console.log("DEFAULT VALUE")
+        break;
+}
