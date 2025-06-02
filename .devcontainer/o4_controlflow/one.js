22aa168f559f3495be12ee2e076bd816d90fb5fd
wishromo <hinasaeed1212@gmail.com>
2025-06-02 06:53:24 +0000

    start with scope of if and concept

---
+++ .devcontainer/o4_controlflow/one.js
@@ -0,0 +1,11 @@
+//if
+if(ture){ // meand the condtion inside the code is true then excute from line no 3 to 5
+
+
+}
+if(false){// mean theat if the condition is false do no texecute from line no 6 to 11
+
+
+
+
+}
