d0ab18e19126ae7b48e65995cc99d2b6512c0a50
wishromo <hinasaeed1212@gmail.com>
2025-06-03 12:27:17 +0000

    changes

--- .devcontainer/04_controlflow/switch.js
+++ .devcontainer/04_controlflow/switch.js
@@ -6,6 +6,7 @@
 //     default:
 //         break;
 // }//use for long checking like months we can not write if else for every month
+//use for redux 
 // 
 const month="march"
 switch (month) {
@@ -16,7 +17,7 @@ switch (month) {
       console.log("feb")
         break;
     case "march":
-        console.log("marcg")
+        console.log("marcH")
         break;
     case "april":
         console.log("april")
@@ -31,4 +32,28 @@ switch (month) {
     default:
         console.log("DEFAULT VALUE")
         break;
+}
+const month2=4
+switch (month2) {
+    case 1:
+        console.log("january");
+        
+        break;
+    case 2:
+        console.log("februray");
+        
+        break;
+    case 3:
+        console.log("march");
+        
+        break;
+    case 4:
+        console.log("april");
+        
+       // break;  // if the key matcif tif we comment one break it will excute the rest of the code after it except default
+
+    default:
+        console.log("month not found");
+        
+        break; 
 }
