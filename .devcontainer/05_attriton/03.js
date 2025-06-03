906a3ec4a42ad779839db51ad7dae0304820e8bb
wishromo <hinasaeed1212@gmail.com>
2025-06-03 19:21:11 +0000

    for of loop

---
+++ .devcontainer/05_attriton/03.js
@@ -0,0 +1,34 @@
+// for of
+// array specific loop
+const arr=[1,2,3,4,5]
+// ["","",""]
+// [{},{},{}]
+// for (const num of arr ){
+//     console.log(num); 
+    
+// }
+// const greeting="hello world"
+// for (const greet of greeting) {
+//     console.log(` each character ${greet}`);
+// }
+//Maps
+ const map = new Map()
+map.set('pak',"pakistan")
+map.set('in',"india")
+map.set('fr',"france")
+map.set('pak',"pakistan")       //it would not print this value because dupliation is not allowed and it ignores it usually use in database
+console.log(map);
+
+// for (const key of map)//   //it will print all the aray
+// for (const [key,value] of map ) {       //give the key of array
+//     console.log(key,':',value)    
+ 
+// }
+const myObject={
+"game1":"NFS",
+"game2":"spiderman"
+ }
+//  for (const [element,value] of myObject ){. //object cannot be access like this
+//     console.log(key,':',value)    
+ 
+//  }
