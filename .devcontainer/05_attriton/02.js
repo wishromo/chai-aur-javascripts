eeb94ad5cb4dda720eb8073b1fcbfe79c578591a
wishromo <hinasaeed1212@gmail.com>
2025-06-03 15:25:08 +0000

    while and do whiel loop

---
+++ .devcontainer/05_attriton/02.js
@@ -0,0 +1,23 @@
+let index=0
+
+// while (codition) {
+    
+// }
+// while (index<= 10) {
+//     console.log(`value of index ${index}`);
+//     // index++      //we. can add 1 everytime in the loop 
+//     index=index+2        //we can add 2 to the variable as well 
+// }
+let myarray=["falsh","batman","superman"]
+
+// while (index < myarray.length) {
+//     console.log(`array value ${myarray[index]}`)
+//     index=index+1
+// }
+    ///do while loop
+    // let score=1
+    
+    do {
+        console.log(`score is ${score}`);
+        score ++
+    } while (score <= 10);
