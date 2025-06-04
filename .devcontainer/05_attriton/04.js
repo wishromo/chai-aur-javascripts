d8b7491fbcb454e9076cb700f05c208f790c226f
wishromo <hinasaeed1212@gmail.com>
2025-06-04 17:37:39 +0000

    for in loop

--- .devcontainer/05_attriton/04.js
+++ .devcontainer/05_attriton/04.js
@@ -1,3 +1,6 @@
+//for in loop for ittrste the object
+
+
 const myObject={
     js:"javascript",
     cpp:"c++",
@@ -5,7 +8,26 @@ const myObject={
     swift:"swift by apple"
 }
 for (const key in myObject) {
-    // console.log(key);
-    // console.log(myObject[key]);// givs keys and object all of it
-    console.log(`${key} shortcut is for ${myObject[key]}`)
+//    console.log(key);
+//     // console.log(myObject[key]);// givs keys and object all of it
+//     console.log(`${key} shortcut is for ${myObject[key]}`)
     }
+    //for in loop on array
+    const myarray=["js,","rb","c++","java"]
+    for (const key in myarray) {
+      {
+    //    console.log(myarray[key]) ;
+            
+        }
+    }
+//  const map = new Map() // cant use for in loop on map because it is not ittrateable
+// map.set('pak',"pakistan")
+// map.set('in',"india")
+// map.set('fr',"france")
+// map.set('pak',"pakistan")   
+// for (const key in map) 
+//     {
+
+//        console.log(map[key]); 
+//     }
+    
